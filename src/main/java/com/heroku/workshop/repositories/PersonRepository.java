6aee0221390a22896a64c18f0ee3692e5c50ae15
rt-heroku <rtorres@heroku.com>
2017-10-16 23:26:03 -0400

    no security

--- src/main/java/com/heroku/workshop/repositories/PersonRepository.java
+++ src/main/java/com/heroku/workshop/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.heroku.workshop.repositories;
  
 import java.util.List;
+import com.heroku.workshop.entities.Person;
  
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
